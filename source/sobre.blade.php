98ed3669d1c7f44634393003c7d51809e111c033
Alfredo Braga <alfredorodruguesbraga@gmail.com>
2021-07-22 22:56:51 -0300

    melhorando a página de perfil e colocando novo icone no header

--- source/sobre.blade.php
+++ source/sobre.blade.php
@@ -1,19 +1,19 @@
 ---
-title: About
-description: A little bit about the site
+title: Sobre
+description: Um pouco sobre Alfredo Braga desenvolvedor web PHP, Linux, Javascript
 ---
 @extends('_layouts.master')
 
 @section('body')
-    <h1>About</h1>
+    <h1>Sobre</h1>
 
-    <img src="/assets/img/about.png"
+    <img src="https://avatars.githubusercontent.com/u/331933?v=4"
         alt="About image"
         class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
+    <p class="mb-6">Meu nome é Alfredo Braga sou desenvolvedor web a cerca de 10 anos, trabalho com <b>Linux, PHP e Javascript.</b></p>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
+    <p class="mb-6">Atualmente ando me aventurando no <b>Python</b> e estou aprendedo a usar o <b>Docker</b>.</p>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+    <p class="mb-6">Nesse espaço espero compartilhar um pouco do que aprendi ao longo desses anos de carreira.</p>
 @endsection
